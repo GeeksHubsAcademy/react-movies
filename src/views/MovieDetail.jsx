97eb079de978c50ad651610e01906694e7d77759
jupegarnica <jupegarnica@gmail.com>
2019-04-18 18:03:35 +0200

    style improvements

--- src/views/MovieDetail.jsx
+++ src/views/MovieDetail.jsx
@@ -9,67 +9,73 @@ class MovieDetail extends React.Component {
   state = {
     movie: null,
     similar: [],
+    id:null
   };
-  componentDidMount() {
+  getData() {
     const url = `https://api.themoviedb.org/3/movie/${this.props.id}?api_key=323112ea2281b9eb70f319f4df422c6b&language=en-US`;
     const similar = `https://api.themoviedb.org/3/movie/${this.props.id}/similar?api_key=323112ea2281b9eb70f319f4df422c6b&language=en-US`;
 
     Axios.get(url)
-      .then(res => this.setState({ movie: res.data }))
+      .then(res => this.setState({ movie: res.data, id: this.props.id }))
       .catch(() => this.props.navigate('/notFound'));
 
     Axios.get(similar)
       .then(res => {
-        console.log(res.data);
-
         this.setState({ similar: res.data.results });
       })
   }
 
   render() {
-    let { movie } = this.state;
 
-    if (!movie) return <h1>loading</h1>;
-    console.log(movie);
+    if(this.props.id !== this.state.id ) {
+      this.getData();
+       return <h1>loading</h1>;
+    } else {
+      let { movie } = this.state;
 
-    let { adult, vote_count, vote_average, homepage, title, backdrop_path, overview, genres, budget, revenue, popularity, release_date, poster_path } = movie;
-    return (
-      <section className='movieDetail' style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w600_and_h900_bestv2/${backdrop_path})` }}>
-        <div className='movie'>
-          <img className='poster' src={'https://image.tmdb.org/t/p/w600_and_h900_bestv2/' + poster_path} alt='' />
+      if (!movie)  return <h1>loading</h1>;
 
-          <div className='data'>
-            <h1>{title}</h1>
-            <h1>{adult ? '♦️♦️♦️' : ''}</h1>
-            <div className='release'>{release_date}</div>
-            <a href={homepage} target='_blank' rel='noopener noreferrer'>
-              {homepage}
-            </a>
-            <p>{overview}</p>
-            <div className='genres'>
-              {genres.map(el => (
-                <span className='genre' key={el.id}>
-                  {el.name}
-                </span>
-              ))}
-            </div>
+      let { adult, vote_count, vote_average, homepage, title, backdrop_path, overview, genres, budget, revenue, popularity, release_date, poster_path } = movie;
+      return (
+        <section className='movieDetail' style={{ backgroundImage: `url(https://image.tmdb.org/t/p/w600_and_h900_bestv2/${backdrop_path})` }}>
+          <div className='movie'>
+            <img className='poster' src={'https://image.tmdb.org/t/p/w300/' + poster_path} alt='' />
 
-            <div className='moreInfo'>
-              <div className='vote_average'>
-                Vote average: <span className='big'>{vote_average}</span>
+            <div className='data'>
+              <h1>{title}</h1>
+              <h1>{adult ? '♦️♦️♦️' : ''}</h1>
+              <div className='release'>{release_date}</div>
+              <a href={homepage} target='_blank' rel='noopener noreferrer'>
+                {homepage}
+              </a>
+              <p>{overview}</p>
+              <div className='genres'>
+                {genres.map(el => (
+                  <span className='genre' key={el.id}>
+                    {el.name}
+                  </span>
+                ))}
               </div>
-              <div className='vote_count'>Vote count: {vote_count}</div>
-              <div className='popularity'>Popularity: {popularity}</div>
-              <div className='budget'>Budget: $ {budget}</div>
-              <div className='revenue'>Revenue: $ {revenue}</div>
+
+              <div className='moreInfo'>
+                <div className='vote_average'>
+                  Vote average: <span className='big'>{vote_average}</span>
+                </div>
+                <div className='vote_count'>Vote count: {vote_count}</div>
+                <div className='popularity'>Popularity: {popularity}</div>
+                <div className='budget'>Budget: $ {budget}</div>
+                <div className='revenue'>Revenue: $ {revenue}</div>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='related'>
-          <MovieDisplay movies={this.state.similar} title='Related:' />
-        </div>
-      </section>
-    );
+          <div className='related'>
+            <MovieDisplay movies={this.state.similar} title='Related:' />
+          </div>
+        </section>
+      );
+    }
+
+
   }
 }
 
