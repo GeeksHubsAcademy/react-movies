996cd3ce8de64779b6a29314c47f4e9a9866c97d
jupegarnica <jupegarnica@gmail.com>
2019-05-08 14:00:52 +0200

    build

---
+++ docs/precache-manifest.0db7596af3217afc2fb315bc359581f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bbca017281ec0571085",
+    "url": "/react-movies/static/css/main.1912c682.chunk.css"
+  },
+  {
+    "revision": "1bbca017281ec0571085",
+    "url": "/react-movies/static/js/main.df8edbc9.chunk.js"
+  },
+  {
+    "revision": "c3e02919837f20e51d96",
+    "url": "/react-movies/static/js/runtime~main.6d17421d.js"
+  },
+  {
+    "revision": "c22b969e803f547100e8",
+    "url": "/react-movies/static/js/2.e7e8f5fa.chunk.js"
+  },
+  {
+    "revision": "04bebe42bacb913caffa6ae3d0a033e9",
+    "url": "/react-movies/index.html"
+  }
+];
